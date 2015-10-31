3418526988fa045c7ca6eaab6363127923bc056c
cahyasetya <cahya.setyaa@gmail.com>
2015-10-31 10:01:16 +0700

    Mengedit mencari bilangan prima. Perubahan: Setiap angka mempunyai thread sendiri dan setiap loop tidak perlu wait, wait dilakukan di luar loop sehingga loop selanjutnya tidak perlu menunggu loop sekarang, jadi waktu lebih efektif.

--- Tugas2.c
+++ Tugas2.c
@@ -4,46 +4,45 @@
 #include<stdlib.h>
 
 typedef struct{
-    char *input;
+    int input;
     int *counter;
 } arguments;
 
 void *cek(void *args)
 {
     arguments *arg=args;
-    int c=atoi(arg->input);
 	int flag=0;
 	int i;
-    if(c<=1) flag=1;
-	else if(c==2) flag=0;
-	else{
-		for(i=2; i<c; i++)
+    if(arg->input<=1) return;
+	else if (arg->input>2){
+		for(i=2; i<arg->input; i++)
 		{
-			if(c%i==0) flag=1;
+			if(arg->input%i==0) return;
 		}
 	}
-	if(flag==0)
-        (*(arg->counter))++;
+    (*(arg->counter))++;
+
 	free(arg);
 }
 
 int main(){
-	char input[10];
-	pthread_t t[10];
-	printf("Masukkan input: ");
-	scanf("%[^\n]", input);
-	char *coba=strtok(input, " ");
-	int i=1;
+	int input;
+	pthread_t *t;
+	printf("Masukkan n: ");
+	scanf("%d", &input);
+	t=malloc(sizeof(pthread_t)*input);
 	int counter=0;
-	while(coba!=NULL)
+	int i;
+	for(i=1; i<input; i++)
 	{
         arguments *arg=malloc(sizeof(*arg));
-        arg->input=coba;
+        arg->input=i;
         arg->counter=&counter;
-        pthread_create(&t[i], NULL, cek, arg);
-        coba=strtok(NULL, " ");
-        i++;
+        pthread_create(&t[i-1], NULL, cek, arg);
     }
+    for(i=0;i<input-2; i++)
+    pthread_join(t[i], NULL);
     printf("Terdapat %d bilangan prima\n", counter);
+    free(t);
 	return 0;
 }
