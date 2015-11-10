fe6397b48d5b05916ec59a1cd4d66e6440e7da21
Cahya Setya <cahya.setyaa@gmail.com>
2015-11-10 21:08:35 +0700

    merapikan kode dan mengubah eksekusi cd tanpa parameter

--- shell_ver.lain.c
+++ shell_ver.lain.c
@@ -7,23 +7,25 @@
 #include <sys/wait.h>
 
 void do_the_job(char **cmd, int bg){
-	int p_pid, i;
-	p_pid = fork();
-
-	if(bg == 1){
-		if(p_pid == 0){ 
-			execvp(cmd[0], cmd);			
-		}
-	}
-	else if(p_pid == 0) execvp(cmd[0], cmd);
-	else {
-		waitpid(p_pid, &i, 0);
+	pid_t pid;
+	int i;
+	pid = fork();
+	if(pid==0)
+        execvp(cmd[0], cmd);
+	else{
+        if(bg==0) wait(0);
 	}
 }
 
 void change_direction(char *cmd_run[]){
 	if(cmd_run[1] != NULL) chdir(cmd_run[1]);
-	else chdir("..");
+	else
+	{
+        char directoryofuser[10]="/home/";
+        char *user=getlogin();
+        strcat(directoryofuser, user);
+        chdir(directoryofuser);
+    }
 }
 
 void ignore(int signal){
@@ -32,11 +34,11 @@ void ignore(int signal){
         username = getlogin();
 	getcwd(cwd, sizeof(cwd));
 
-	fprintf(stderr,"\n\x1b[31m%s@%s \x1b[34m%s &: \x1b[0m", username, hostname, cwd);
+	fprintf(stderr,"\n\x1b[31m%s@%s \x1b[34m%s : \x1b[0m", username, hostname, cwd);
 }
 
 int main(){
-	char *username, hostname[100], cwd[1024], cmd[1024], *cmd_run[50], temp[100];
+	char *username, hostname[100], cwd[1024], sintaks[100000], *cmd_run[50], temp[100];
 	int i, bg = 0, length;
 	signal(SIGINT, ignore);
 	signal(SIGTSTP, ignore);
@@ -46,33 +48,23 @@ int main(){
         	username = getlogin();
 		getcwd(cwd, sizeof(cwd));
 
-		printf("\x1b[31m%s@%s \x1b[34m%s &: \x1b[0m", username, hostname, cwd);
+		printf("\x1b[31m%s@%s \x1b[34m%s : \x1b[0m", username, hostname, cwd);
+        scanf("%[^\n]", sintaks);
+        getchar();
+        if(sintaks[0]==0) continue;
+        else{
+            if(sintaks[strlen(sintaks)-1]==38) bg=1;
+            cmd_run[0] = strtok(sintaks, " &");
 
-		if(fgets(cmd, sizeof(cmd), stdin) == NULL) break;
-		if(cmd[strlen(cmd)-1] == '\n') cmd[strlen(cmd)-1] = '\0';
+            for(i = 1; i < 100; i++)
+                cmd_run[i] = strtok(NULL, " &");
 
-		cmd_run[0] = strtok(cmd, " ");
-		if(cmd_run[0] == NULL) break;
-		
-		for(i = 1; i < 100; i++){
-			cmd_run[i] = strtok(NULL, " ");
-			if(cmd_run[i] == NULL){
-				strcpy(temp, cmd_run[i-1]);
-				length = strlen(temp);
-				
-				if(temp[length-1] == '&'){
-					temp[length-1] = '\0';
-					strcpy(cmd_run[i-1],temp);
-					bg = 1;
-				}
-				break;
-			}
-		}
-
-		if(strcmp(cmd_run[0], "cd") == 0) change_direction(cmd_run);
-		else if (strcmp(cmd_run[0],"exit") == 0) break;
-		else do_the_job(cmd_run, bg);
-		bg = 0;
+            if(strcmp(cmd_run[0], "cd") == 0) change_direction(cmd_run);
+            else if (strcmp(cmd_run[0],"exit") == 0) break;
+            else do_the_job(cmd_run, bg);
+            bg = 0;
+        }
+        sintaks[0]=0;
 	}
 	printf("\n");
 }
